9404fb1026cbc36c48d90fbb3cb005d772508f4c
Ervin <edrenchev@gmail.com>
2017-02-01 10:02:30 +0200

    commit

---
+++ config/autoload/site.global.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ervin
+ * Date: 01/02/2017
+ * Time: 09:59
+ */
+
+return [
+	'site_config' => [
+		'bg' => 'Bulgarian',
+		'en' => 'English',
+	],
+];
